5bee60d3753561257a97ef4594a13b12d08e9cdc
hajame <harri.mehtala@helsinki.fi>
2022-02-10 18:53:13 +0200

    ex2 RESET - 2nd run start - green

--- ex2-small-steps/src/prices.mjs
+++ ex2-small-steps/src/prices.mjs
@@ -1,6 +1,5 @@
 import "./polyfills.mjs";
 import express from "express";
-import { Temporal } from "@js-temporal/polyfill";
 
 // Refactor the following code to get rid of the legacy Date class.
 // Use Temporal.PlainDate instead. See /test/date_conversion.spec.mjs for examples.
@@ -26,7 +25,7 @@ function createApp(database) {
 
   function parseDate(dateString) {
     if (dateString) {
-      return new Temporal.PlainDate.from(dateString);  // completed first refactoring BOTTOM -> TOP
+      return new Date(dateString);
     }
   }
 
@@ -77,14 +76,19 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.dayOfWeek === 1;
+    return date.getDay() === 1;
   }
 
-  function isHoliday(date) {    
+  function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      let holiday = Temporal.PlainDate.from(row.holiday);
-      if (date && date.equals(holiday)) {
+      let holiday = new Date(row.holiday);
+      if (
+        date &&
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
+      ) {
         return true;
       }
     }
