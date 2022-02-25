79c1a9e8c3f42f1275ac7a9ee7f508aceba13f8e
hajame <harri.mehtala@helsinki.fi>
2022-02-25 11:40:28 +0200

    3rd run - green - step 2

--- ex2-small-steps/src/prices.mjs
+++ ex2-small-steps/src/prices.mjs
@@ -83,10 +83,10 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = new Date(row.holiday);
-      let aHoliday = new Temporal.PlainDate.from(row.holiday);
+      let aHoliday = Temporal.PlainDate.from(row.holiday);
       if (
         date &&
-        date.getFullYear() === holiday.getFullYear() &&
+        date.getFullYear() === aHoliday.getISOFields().isoYear &&
         date.getMonth() === holiday.getMonth() &&
         date.getDate() === holiday.getDate()
       ) {
