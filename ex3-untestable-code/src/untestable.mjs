1ac95804d1df55bf8f6c25b5c0b09c090155d20c
hajame <harri.mehtala@helsinki.fi>
2022-03-20 11:28:55 +0200

    ex3 - dependency on files - red

--- ex3-untestable-code/src/untestable.mjs
+++ ex3-untestable-code/src/untestable.mjs
@@ -1,9 +1,18 @@
+import fs from "fs";
+
 export function sum(a, b) {
   return a + b;
 }
 
 export function now() {
   let time = new Date(Date.now()).toISOString();
-  console.log(time);
   return time;
+}
+
+export function readFile() {
+  try {
+    return fs.readFileSync("file.txt", "ascii");
+  } catch (err) {
+    console.log(err);
+  }
 }
--- ex3-untestable-code/test/untestable.test.mjs
+++ ex3-untestable-code/test/untestable.test.mjs
@@ -1,8 +1,12 @@
 import { expect } from "chai";
-import { sum, now } from "../src/untestable.mjs";
+import { sum, readFile, now } from "../src/untestable.mjs";
 
 describe("Testing the untestable", () => {
-  it("Example test", () => {
+  it("Test we depend on time", () => {
     expect(now()).to.equal(new Date(Date.now()).toISOString());
+  });
+  it("Test we depend on files", () => {
+    console.log(readFile());
+    expect(readFile()).to.equal(new Date(Date.now()).toISOString());
   });
 });
