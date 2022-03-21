019876c480d1e9f27c8ef5e7ed68e8737fd634e4
hajame <harri.mehtala@helsinki.fi>
2022-03-21 08:48:08 +0200

    ex4 - sulfuras tests complete, improve normal item tests, rename test cases

--- ex4-legacy-code/test/test_gilded_rose.js
+++ ex4-legacy-code/test/test_gilded_rose.js
@@ -3,51 +3,76 @@ var { Shop, Item } = require("../src/gilded_rose.js");
 
 describe("Gilded Rose", function () {
   describe("Normal items", function () {
-    it("item foo with 1 quality", function () {
-      const gildedRose = new Shop([new Item("foo", 0, 1)]);
+    it("before best before date, worsen quality by 1", function () {
+      const gildedRose = new Shop([new Item("foo", 30, 3)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(2);
+    });
+    it("before best before date, quality does not go below 0", function () {
+      const gildedRose = new Shop([new Item("foo", 30, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(0);
     });
-    it("item foo with -1 sellIn", function () {
+    it("past best before date, worsen quality by 2", function () {
+      const gildedRose = new Shop([new Item("foo", -1, 3)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(1);
+    });
+    it("past best before date, quality does not go below 0", function () {
       const gildedRose = new Shop([new Item("foo", -1, 1)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(0);
     });
   });
   describe("Aged Brie", function () {
-    it("0 quality, 0 sellIn", function () {
+    it("if past best before date and less than 50 quality, improve quality", function () {
       const gildedRose = new Shop([new Item("Aged Brie", 0, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(2);
     });
-    it("50+ quality", function () {
+    it("if 50+ quality, quality remains", function () {
       const gildedRose = new Shop([new Item("Aged Brie", 0, 100)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(100);
     });
   });
   describe("Backstage passes to a TAFKAL80ETC concert", function () {
-    it("0 quality, 10 sellIn", function () {
+    it("0 quality, 10 best before date", function () {
       const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(2);
     });
-    it("0 quality, 5 sellIn", function () {
+    it("0 quality, 5 best before date", function () {
       const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(3);
     });
-    it("if sellIn 0, quality is 0", function () {
+    it("value after best before date is 0", function () {
       const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 100)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(0);
     });
   });
-  describe("Sulfuras, Hand of Ragnaros", function () {
-    it("0 quality, 0 sellIn", function () {
-      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 10, 0)]);
+  describe("Sulfuras, Hand of Ragnaros does not weaken", function () {
+    it("0 quality, 0 best before date", function () {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 0)]);
       const items = gildedRose.updateQuality();
       expect(items[0].quality).to.equal(0);
+    });
+    it("100 quality, 0 best before date", function () {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 100)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(100);
+    });
+    it("0 quality, 100 best before date", function () {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 100, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(0);
+    });
+    it("100 quality, 100 best before date", function () {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 100, 100)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).to.equal(100);
     });
   });
 });
